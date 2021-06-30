25992814068594ca7fb4c81f2fbb85cff275c826
pakkiii <80090340+pakkiii@users.noreply.github.com>
2021-06-30 13:14:02 +0900

    first commit
    投稿モデルの作成
    マイグレーションファイル
    テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_023829) do
+ActiveRecord::Schema.define(version: 2021_06_30_040751) do
+
+  create_table "lives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "bad_thing", null: false
+    t.text "hope", null: false
+    t.integer "month_id", null: false
+    t.integer "how_long_id", null: false
+    t.integer "want_id", null: false
+    t.integer "tired_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lives_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_06_30_023829) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lives", "users"
 end
