f7ba176f3c687ffd25c9efd8cd4d9168a8af4056
pakkiii <80090340+pakkiii@users.noreply.github.com>
2021-07-29 15:21:54 +0900

    authenticate_user
    エラー解決
    グラフを一時消去

--- app/controllers/lights_controller.rb
+++ app/controllers/lights_controller.rb
@@ -1,10 +1,11 @@
 class LightsController < ApplicationController
+
+  before_action :authenticate_user!, only: :index
+
   def index
     @lights = current_user.passive_lights
     @lights.where(checked: false).each do |light|
       light.update_attributes(checked: true)
     end
   end
-
-  
 end
