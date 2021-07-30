9d008150ae3951d616fc736cfb48fb6200a2e3a7
pakkiii <80090340+pakkiii@users.noreply.github.com>
2021-07-30 17:14:24 +0900

    commit

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,5 +1,4 @@
 class LivesController < ApplicationController
-  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :show]
 
   def index
@@ -69,11 +68,7 @@ class LivesController < ApplicationController
 
   private
 
-  def move_to_index
-    if current_user.id != @life.user.id
-      redirect_to action: :index 
-    end
-  end
+  
 
   def life_params
     params.require(:life).permit(:title, :bad_thing, :hope, :tired_id, :how_long_id, :want_id, :month_id).merge(user_id: current_user.id)
